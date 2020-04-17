3d0e439a5dab6d4568060efbf8d07f5dc0503d83
Advaith Bhat <advaithbhat9@gmail.com>
2020-04-17 10:46:41 +0000

    op3: Add prebuilt CameraGo
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- device.mk
+++ device.mk
@@ -559,6 +559,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf \
     android.hardware.wifi@1.0-service
 
+# Camera
+PRODUCT_PACKAGES += \
+    CameraGo
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
