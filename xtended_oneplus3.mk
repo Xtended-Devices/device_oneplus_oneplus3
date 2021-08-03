017feaf339b6f002f69ef4d92f27772192a0031d
SuperDroidBond <superdroidbond@yahoo.com>
2021-08-03 21:33:36 +0530

    update Maintainer Tag n Donation URL

--- xtended_oneplus3.mk
+++ xtended_oneplus3.mk
@@ -35,9 +35,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 TARGET_VENDOR := oneplus
 
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
+
 XTENDED_BUILD_TYPE := OFFICIAL
 
-# Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xtended.maintainer=SuperDroidBond
+XTENDED_BUILD_MAINTAINER := SuperDroidBond
 
+XTENDED_BUILD_DONATE_URL := https://www.paypal.me/superdroidbond
